6e9871725c634900d0c72fb1097a2fdf91e4a12c
lukap <lukap2211@gmail.com>
2013-08-20 12:45:27 +0100

    Login screen update

--- admin/include/login_form.php
+++ admin/include/login_form.php
@@ -4,12 +4,25 @@
     // echo "<br />passs: $password";
     ?>
 
-    <form class="form-signin" method="POST" action="index.php">
-        <h2 class="form-signin-heading">Please sign in</h2>
-        <input type="text" class="input-medium" placeholder="Username" name="username" value="<?php ($username !== NULL) ? "" : $username ; echo $username; ?>" >
-        <input type="password" class="input-medium" placeholder="Password" name="password" value="<?php ($password !== NULL) ? "" : $password ; echo $password; ?>">
-        <label class="checkbox">
-            <input type="checkbox" value="remember-me"> Remember me
-        </label>
-        <button class="btn btn-large btn-primary" type="submit">Sign in</button>
+    <form class="form-horizontal" method="POST" action="index.php">
+        <!-- <h2 class="form-signin-heading">Please sign in</h2> -->
+        <legend>Please sign in</legend>
+        <div class="control-group">
+            <label class="control-label" for="inputUsername">Password</label>
+            <div class="controls">
+                <input id="inputUsername" type="text" class="input-medium" placeholder="Username" name="username" value="<?php ($username !== NULL) ? "" : $username ; echo $username; ?>" >
+            </div>
+        </div>
+        <div class="control-group">
+            <label class="control-label" for="inputPassword">Password</label>
+            <div class="controls">
+                <input id="inputPassword" type="password" class="input-medium" placeholder="Password" name="password" value="<?php ($password !== NULL) ? "" : $password ; echo $password; ?>" >
+            </div>
+        </div>
+        <div class="control-group">
+            <div class="controls">
+                <button class="btn btn-large btn-primary" type="submit">Sign in</button>
+            </div>
+        </div>
+
     </form>
