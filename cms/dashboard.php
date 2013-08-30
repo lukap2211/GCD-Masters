d47ab679ce400d6b206a504f2b6e80d9c06e4224
lukap <lukap2211@gmail.com>
2013-08-30 20:13:13 +0100

    Google Analytics CMS

--- cms/dashboard.php
+++ cms/dashboard.php
@@ -166,6 +166,18 @@ http://www.apache.org/licenses/LICENSE-2.0.txt
     <script src="js/bootstrap.js"></script>
     <script src="js/cms.js"></script>
 
+    <!-- GOOGLE ANALYTICS  -->
+    <script>
+        (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+        (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+        m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+        })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+        ga('create', 'UA-43664547-2', 'lukap.info');
+        ga('send', 'pageview');
+
+    </script>
+
 </body>
 
 </html>
