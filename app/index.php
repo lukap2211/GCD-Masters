75dafbd7468ee8fb2dd9d650ab3767a4b0c4d609
lukap <lukap2211@gmail.com>
2013-08-30 18:18:09 +0100

    Google Analitics

--- app/index.php
+++ app/index.php
@@ -183,6 +183,18 @@ http://www.apache.org/licenses/LICENSE-2.0.txt
     <script src="..//assets/js/jquery-1.10.1.min.js"></script>
     <script src="js/app.js"></script>
 
+    <!-- GOOGLE ANALITICS -->
+    <script>
+      (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+      (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+      m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+      })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+      ga('create', 'UA-43664547-1', 'lukap.info');
+      ga('send', 'pageview');
+
+    </script>
+
 </body>
 
 </html>
