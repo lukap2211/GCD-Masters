76702a87f7adaf05ece7c44e642c4a4fd8535533
lukap <lukap2211@gmail.com>
2013-08-31 10:09:49 +0100

    Landing page

---
+++ index.php
@@ -0,0 +1,67 @@
+<!DOCTYPE html>
+
+<!--
+
+Copyright 2013 Luka Puharic
+http://www.apache.org/licenses/LICENSE-2.0.txt
+
+ -->
+
+<html>
+<head>
+
+    <title>GCD Geo CMS App</title>
+
+    <!-- Responsive CSS -->
+    <link type="text/css" rel="stylesheet" href="css/style.css" media="screen" />
+
+    <!-- Roboto - Free Font from Google -->
+    <link href='http://fonts.googleapis.com/css?family=Roboto:400,700' rel='stylesheet' type='text/css'>
+
+    <!-- icons -->
+    <link rel="icon" href="../assets/icons/favicon-app.ico" type="image/x-icon">
+    <link rel="shortcut icon" href="../assets/icons/favicon-app.ico" type="image/x-icon">
+    <link rel="apple-touch-icon" href="../assets/icons/touch-icon-iphone.png" />
+    <link rel="apple-touch-icon" sizes="72x72" href="../assets/icons/touch-icon-ipad.png" />
+    <link rel="apple-touch-icon" sizes="114x114" href="../assets/icons/touch-icon-iphone-retina.png" />
+    <link rel="apple-touch-icon" sizes="144x144" href="../assets/icons/touch-icon-ipad-retina.png" />
+
+    <link rel="apple-touch-startup-image" href="../assets/icons/touch-icon-ipad-retina.png">
+
+    <style type="text/css">
+        body {margin: 0; padding:0; background: url(app/img/bg.png); font-size: 30; font-family: "Roboto", Helvetica, Arial, serif; color:#A1A1A1; border: none; text-align: center;}
+        nav {position: absolute; left: 0; right: 0; top: 0; bottom: 0; margin: auto; height: 150px; width: 300px; border:5px dashed #A1A1A1;}
+        a {margin: 5px; }
+        .button,
+        .button:active {font-weight: 400; -webkit-border-radius: 4px; background-color: #009FDD; text-shadow: 0 1px #0072C7; color: #FFF; border:0; cursor: pointer; border-radius: 4px; padding: 0.3em 0.8em; display: inline-block; line-height: 1.6em; font-size: 1.3em; text-decoration: none; letter-spacing: 0.1em; }
+        .button:hover {background-color: #2a2a2a; color: #F1F1F1; text-shadow: 0 -1px 1px #0072C7;}
+    </style>
+
+</head>
+
+<body>
+
+    <!-- MAIN NAVIGATION -->
+    <nav>
+        <h1> GCD MScADM</h1>
+        <p>
+            <a href="cms/" class="button">CMS</a>
+            <a href="app/" class="button">App</a>
+        </p>
+    </nav>
+
+    <!-- GOOGLE ANALITICS -->
+    <script>
+      (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+      (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+      m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+      })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+      ga('create', 'UA-43664547-1', 'lukap.info');
+      ga('send', 'pageview');
+
+    </script>
+
+</body>
+
+</html>
